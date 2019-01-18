692dba4efb8c4b43e6c0e5707e848d53c8ee0cb5
Steve Parker <steve.parker@tescobank.com>
2019-01-18 16:13:43 +0000

    basic working codebuild

--- build/deploy.sh
+++ build/deploy.sh
@@ -5,13 +5,13 @@ stack=$(aws cloudformation describe-stacks --stack-name $stack_name --query Stac
 if [ -z "$stack" ]
 then
       echo "$stack_name does not exist, creating..."
-      aws cloudformation create-stack --stack-name $stack_name --template-body file://cloudformation/jobcreator.yml
+      aws cloudformation create-stack --stack-name $stack_name --template-body file://cloudformation/jobcreator.yml --capabilities CAPABILITY_IAM
       echo "Waiting on stack to create..."
       aws cloudformation wait stack-create-complete --stack-name $stack_name
       echo "Stack created."
 else
       echo "$stack_name does exist, updating..."
-      aws cloudformation update-stack --stack-name $stack_name --template-body file://cloudformation/jobcreator.yml
+      aws cloudformation update-stack --stack-name $stack_name --template-body file://cloudformation/jobcreator.yml --capabilities CAPABILITY_IAM
       if [[ $? != 0 ]]; then exit $?; fi
       echo "Waiting on stack to update..."
       aws cloudformation wait stack-update-complete --stack-name $stack_name
